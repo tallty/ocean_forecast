6bcd0f52e91b1f0be43241f7690028f7a367b845
liyijie <liyijie825@gmail.com>
2016-10-02 00:30:35 +0800

    last_proc_time exception proc

--- app/models/nafp.rb
+++ app/models/nafp.rb
@@ -14,7 +14,7 @@ module Nafp
 
     remote_files = []
     ["#{yestoday_dir}/00", "#{yestoday_dir}/12", "#{today_dir}/00", "#{today_dir}/12"].each do |dir|
-      last_proc_time = Time.zone.parse MultiJson.load($redis.hget("last_proc_time", self.class.to_s)) rescue Time.zone.now-1.day
+      last_proc_time = ( Time.zone.parse MultiJson.load($redis.hget("last_proc_time", self.class.to_s)) rescue Time.zone.now-1.day )
       @connection.chdir dir rescue next
       files = self.ls @file_pattern
       files.each do |file|
