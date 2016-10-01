4d40123b7075ec1bdcb828dc79ec3d91d8837db5
liyijie <liyijie825@gmail.com>
2016-10-01 23:19:38 +0800

    add nafp

---
+++ app/models/nafp.rb
@@ -0,0 +1,16 @@
+module Nafp
+  include FtpConcern
+
+  def convert_grib_to_netcdf
+    system "#{ENV["wgrib2_path"]/wgrib2} #{grib_filename} -netcdf #{nc_filename}"
+  end
+  
+  def fetch
+    self.connect
+    today = Time.zone.today
+    today_dir = today.strftime('%Y/%m/%d/')
+    @connection.chdir today_dir
+    files = self.ls("")
+    puts "files is:#{files.inspect}"
+  end
+end
---
+++ app/models/nafp/high_warm.rb
@@ -0,0 +1,28 @@
+class Nafp::HighWarm
+  include Nafp
+
+  attr_reader :grib_filename, :nc_filename
+
+  def initialize(grib_filename)
+    @grib_filename = @grib_filename
+    @nc_filename = "#{grib_filename}.nc" 
+    @server = "10.228.2.53"
+    @port = 21
+    @user = "kfhyuser"
+    @password = "0QY7rN86taOq"
+  end
+
+  def extract_to_redis
+    @file = NumRu::NetCDF.open nc_filename
+    _var_name = "TMP_2mb"
+    _var = @file.var _var_name
+    _var.get.each_with_index do |_var_value, _index|
+      _lon_index = 
+      _lat_index = 
+      _redis_key = "#{_var_name}_#{time}"
+      $redis.hset _redis_key, _lon_index
+    end
+  end
+
+  
+end
