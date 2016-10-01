0f8f8dd2f35f43570bd63c081c8f1822d3f68f5e
liyijie <liyijie825@gmail.com>
2016-10-02 00:06:58 +0800

    fix Nafp module fetch feature

--- app/models/nafp.rb
+++ app/models/nafp.rb
@@ -9,8 +9,30 @@ module Nafp
     self.connect
     today = Time.zone.today
     today_dir = today.strftime('%Y/%m/%d/')
-    @connection.chdir today_dir
-    files = self.ls
+    yestoday = today - 1.day
+    yestoday_dir = yestoday.strftime('%Y/%m/%d/')
+
+    remote_files = []
+    ["#{yestoday_dir}/00", "#{yestoday_dir}/12", "#{today_dir}/00", "#{today_dir}/12"].each do |dir|
+      last_proc_time = Time.zone.parse MultiJson.load($redis.hget("last_proc_time", self.class.to_s)) rescue Time.zone.now-1.day
+      @connection.chdir dir
+      files = self.ls @file_pattern
+      files.each do |file|
+        remote_files << File.join(dir, file) if created_at(file) > last_proc_time
+      end
+
+    end
+    
     puts "files is:#{files.inspect}"
+    files
+  end
+
+  def created_at filename
+    time_string = filename.split("_")[-3]
+    Time.zone.parse(time_string)+8.hour
+  end
+
+  def to_datetime_string time
+    time.strftime('%Y%m%d%H')
   end
 end
--- app/models/nafp/high_warm.rb
+++ app/models/nafp/high_warm.rb
@@ -10,6 +10,7 @@ class Nafp::HighWarm
     @port = 21
     @user = "kfhyuser"
     @password = "0QY7rN86taOq"
+    @file_pattern = "high-warms"
   end
 
   def extract_to_redis
