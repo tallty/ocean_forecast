34a3f8ebefac5802deb656406c8b9230c953a9a5
liyijie <liyijie825@gmail.com>
2016-10-14 05:42:29 +0800

    remove nafp module

--- app/models/nafp/high_warm.rb
+++ app/models/nafp/high_warm.rb
@@ -1,26 +1,26 @@
-class Nafp::HighWarm
-  include Nafp
+# class Nafp::HighWarm
+#   include Nafp
 
-  def initialize
-    @server = "10.228.2.53"
-    @port = 21
-    @user = "kfhyuser"
-    @password = "0QY7rN86taOq"
-    @file_pattern = "*high-warms*"
-    @local_dir = "./high-warms"
-  end
+#   def initialize
+#     @server = "10.228.2.53"
+#     @port = 21
+#     @user = "kfhyuser"
+#     @password = "0QY7rN86taOq"
+#     @file_pattern = "*high-warms*"
+#     @local_dir = "./high-warms"
+#   end
 
-  def extract_to_redis
-    @file = NumRu::NetCDF.open nc_filename
-    _var_name = "TMP_2mb"
-    _var = @file.var _var_name
-    _var.get.each_with_index do |_var_value, _index|
-      _lon_index = 
-      _lat_index = 
-      _redis_key = "#{_var_name}_#{time}"
-      $redis.hset _redis_key, _lon_index
-    end
-  end
+#   def extract_to_redis
+#     @file = NumRu::NetCDF.open nc_filename
+#     _var_name = "TMP_2mb"
+#     _var = @file.var _var_name
+#     _var.get.each_with_index do |_var_value, _index|
+#       _lon_index = 
+#       _lat_index = 
+#       _redis_key = "#{_var_name}_#{time}"
+#       $redis.hset _redis_key, _lon_index
+#     end
+#   end
 
   
-end
+# end
--- app/models/nafp/surface_warm.rb
+++ app/models/nafp/surface_warm.rb
@@ -1,52 +1,52 @@
-require "numru/netcdf"
+# require "numru/netcdf"
 
-class Nafp::SurfaceWarm
-  include Nafp
+# class Nafp::SurfaceWarm
+#   include Nafp
 
-  def initialize
-    @server = "10.228.2.53"
-    @port = 21
-    @user = "kfhyuser"
-    @password = "0QY7rN86taOq"
-    @file_pattern = "*surface-warms*"
-    @local_dir = "./surface-warms"
-  end
+#   def initialize
+#     @server = "10.228.2.53"
+#     @port = 21
+#     @user = "kfhyuser"
+#     @password = "0QY7rN86taOq"
+#     @file_pattern = "*surface-warms*"
+#     @local_dir = "./surface-warms"
+#   end
 
-  def proc
-    local_files = fetch
-    local_files.each do |local_file|
-      nc_filename = convert_grib_to_netcdf(local_file)
-      puts "proc nc file #{nc_filename}..."
-      extract_to_redis nc_filename, ["APCP_surface", "TMP_surface"]
-    end
-  end
+#   def proc
+#     local_files = fetch
+#     local_files.each do |local_file|
+#       nc_filename = convert_grib_to_netcdf(local_file)
+#       puts "proc nc file #{nc_filename}..."
+#       extract_to_redis nc_filename, ["APCP_surface", "TMP_surface"]
+#     end
+#   end
 
-  def extract_to_redis nc_filename, vars
-    return if nc_filename.blank?
-    _file = ::NumRu::NetCDF.open nc_filename
-    _origin_time_string = to_datetime_string created_at(nc_filename)
-    _time_string = to_datetime_string updated_at(nc_filename)
-    vars.each do |_var_name|
-      _var = _file.var _var_name
-      _lat_array = []
-      _var.get.each.with_index do |_var_value, _index|
-        _lon_index = _index % 759
-        _lat_index = _index / 759
-        _lat_array = [] if _lon_index == 0
-        _lat_array << _var_value
-        if _lon_index == 758
-          _redis_key = "#{_var_name}_#{_time_string}"
-          $redis.hset _redis_key, _lat_index, _lat_array.to_json
-          $redis.expire _redis_key, 24*3600 #24 hours expire
-        end
-      end
-    end
-    $redis.hset("last_proc_time", self.class.to_s, _origin_time_string)
-  end
+#   def extract_to_redis nc_filename, vars
+#     return if nc_filename.blank?
+#     _file = ::NumRu::NetCDF.open nc_filename
+#     _origin_time_string = to_datetime_string created_at(nc_filename)
+#     _time_string = to_datetime_string updated_at(nc_filename)
+#     vars.each do |_var_name|
+#       _var = _file.var _var_name
+#       _lat_array = []
+#       _var.get.each.with_index do |_var_value, _index|
+#         _lon_index = _index % 759
+#         _lat_index = _index / 759
+#         _lat_array = [] if _lon_index == 0
+#         _lat_array << _var_value
+#         if _lon_index == 758
+#           _redis_key = "#{_var_name}_#{_time_string}"
+#           $redis.hset _redis_key, _lat_index, _lat_array.to_json
+#           $redis.expire _redis_key, 24*3600 #24 hours expire
+#         end
+#       end
+#     end
+#     $redis.hset("last_proc_time", self.class.to_s, _origin_time_string)
+#   end
 
-  def extract_var_to_redis file, var_name
+#   def extract_var_to_redis file, var_name
     
-  end
+#   end
 
   
-end
+# end
