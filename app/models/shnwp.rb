fa590b78307b68a4d1127a891b3aeba1143aa8d7
liyijie <liyijie825@gmail.com>
2016-11-12 13:48:13 +0800

    add shnpw module & needed classes

---
+++ app/models/shnwp.rb
@@ -0,0 +1,11 @@
+module Shnwp
+  include FtpConcern
+
+  def processing?
+      $redis.get("#{self.class.to_s}#processing").present? 
+  end
+
+  def to_datetime_string time
+    time.strftime('%Y%m%d%H')
+  end
+end
