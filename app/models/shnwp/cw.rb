ad6744af48594e66755de94b18f81f720eeba623
liyijie <liyijie825@gmail.com>
2017-01-16 15:21:30 +0800

    change FC & CW ftp server

--- app/models/shnwp/cw.rb
+++ app/models/shnwp/cw.rb
@@ -2,11 +2,11 @@ class Shnwp::Cw
   include Shnwp
 
   def initialize
-    @server = "59.110.141.81"
+    @server = "10.228.18.61"
     @port = 21
     @passive = true
-    @user = "ftptest"
-    @password = "Kjfwzx2016"
+    @user = "unimet"
+    @password = "123456"
     @local_dir = "../sh_weather/public/cw"
     @remote_dir = "/CW"
 
