9d2eef0af157c4ace61aa20662bd5d20ff61eae6
ivan Lan <mumumumushu@gmail.com>
2017-10-26 20:45:34 +0800

    Improve schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,30 +8,30 @@
 set :output, "./log/cron_log.log"
 #
 
-every 10.minutes do
-  runner "Shnwp::Gfs.new.fetch_latest"
-end
+# every 10.minutes do
+#   runner "Shnwp::Gfs.new.fetch_latest"
+# end
 
-every 10.minutes do
-  runner "Shnwp::Hycom.new.fetch_latest"
-end
+# every 10.minutes do
+#   runner "Shnwp::Hycom.new.fetch_latest"
+# end
 
-every 10.minutes do
-  runner "Shnwp::Nww3.new.fetch_latest"
-end
+# every 10.minutes do
+#   runner "Shnwp::Nww3.new.fetch_latest"
+# end
 
-every 10.minutes do
-  runner "Shnwp::Warms.new.fetch_latest"
-end
+# every 10.minutes do
+#   runner "Shnwp::Warms.new.fetch_latest"
+# end
 
-# Learn more: http://github.com/javan/whenever
-every 1.day, :at => '20:25' do
-  runner "Shnwp::Fc.new.fetch_latest"
-end
+# # Learn more: http://github.com/javan/whenever
+# every 1.day, :at => '20:25' do
+#   runner "Shnwp::Fc.new.fetch_latest"
+# end
 
-every 1.day, :at => '20:25' do
-  runner "Shnwp::Cw.new.fetch_latest"
-end
+# every 1.day, :at => '20:25' do
+#   runner "Shnwp::Cw.new.fetch_latest"
+# end
 
 every 10.minutes do
   runner "NABC::Hfradar.new.scan"
